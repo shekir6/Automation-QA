a563e724dae2741bbc2be76f34de8076b3180da2
Shekir Troh <113124593+shekir6@users.noreply.github.com>
2024-01-19 09:02:14 +1200

    Delete 02-Visual Studio - Lab directory

--- 02-Visual Studio - Lab/03-customerInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿string firstName = Console.ReadLine();
-string lastName = Console.ReadLine();
-string email = Console.ReadLine();
-
-
-Console.Write("Customer: ");
-Console.Write(firstName);
-Console.Write(" " + lastName);
-Console.Write(" (" + email + ")");
--- 02-Visual Studio - Lab/05-townInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿string townName = Console.ReadLine();
-string country = Console.ReadLine();
-Console.Write(townName + " is in ");
-Console.Write(country);
-
-//"{town name} is in {country}"
-
-
-
