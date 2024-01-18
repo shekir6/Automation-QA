90424bd2ff7640b104845708d6e4598fb90ba1d4
Shekir Troh <113124593+shekir6@users.noreply.github.com>
2024-01-19 09:02:33 +1200

    Delete 05-Simple Calculations directory

--- 05-Simple Calculations/01-convertorUsdToEuro.cs
+++
@@ -1,5 +0,0 @@
-﻿double dolar = double.Parse(Console.ReadLine());
-
-double dolarToEuro = dolar * 0.88;
-
-Console.WriteLine($"{dolarToEuro:F2}");
--- 05-Simple Calculations/06-mandatoryLiterature.cs
+++
@@ -1,8 +0,0 @@
-﻿int numOfPages = int.Parse(Console.ReadLine());
-int pages = int.Parse(Console.ReadLine());
-int numOfDays = int.Parse(Console.ReadLine());
-
-int totalReadingTime = numOfPages / pages;
-int hourPerDay = totalReadingTime / numOfDays;
-
-Console.WriteLine(hourPerDay);
