aaf9c1b45e87da9eb95e04fa16d47deba144c470
Shekir Troh <113124593+shekir6@users.noreply.github.com>
2024-01-25 08:28:03 +1200

    Add files via upload

---
+++ Programming Fundamentals and Unit Testing - януари 2024/07-Simple Conditional Statements/08-ticketPrice.cs
@@ -0,0 +1,14 @@
+﻿string ticetType = Console.ReadLine();
+
+if (ticetType == "student")
+{
+    Console.WriteLine("$1.00");
+}
+else if (ticetType == "regular")
+{
+    Console.WriteLine("$1.60");
+}
+else
+{
+    Console.WriteLine("Invalid ticket type!");
+}
