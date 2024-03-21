4e3539aa177e5e0f160fa5282a4173472588fbfc
Shekir Troh <113124593+shekir6@users.noreply.github.com>
2024-03-22 06:56:32 +1200

    Add files via upload

---
+++ Programming Advanced for QA/02-Regular Expressions - Lab/02-matchPhoneNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string text = Console.ReadLine();
+
+        
+        string pattern = @"\+359( |-)2\1\d{3}( |-)\d{4}\b";
+
+        
+        MatchCollection matches = Regex.Matches(text, pattern);
+
+        
+        foreach (Match match in matches)
+        {
+            Console.Write(match.Value);
+            if (match != matches[matches.Count - 1])
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
